18d34961601d0b6cd798ee0914759faa9c8c437c
Wanderson Maikon <wandersonmaikon@gmail.com>
2026-01-22 20:22:56 -0400

    Funcionalidade: Adição de visualizações para gerenciamento de usuários e perfis
    - Criado o arquivo perfil-lista.ejs para gerenciar perfis de acesso com botões de busca e ação.
    
    - Adicionado o arquivo usuario-form.ejs para criação de usuários com validação de formulário e notificações do SweetAlert.
    
    - Implementado o arquivo usuario-lista.ejs para exibir usuários com filtros, ações e um modal para edição de permissões.

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const verificarAutenticacao = (req, res, next) => {
 router.get("/login", (req, res) => {
     const message = req.session.message || "";
     req.session.message = "";
-    res.render("login", { message });
+    res.render("auth/auth-login", { message });
 });
 
 router.post("/login", (req, res) => {
@@ -25,49 +25,49 @@ router.post("/login", (req, res) => {
 });
 
 router.get("/dashboard", verificarAutenticacao, (req, res) => {
-    res.render("dashboard", {
+    res.render("dashboard/index", {
         user: req.session.user,
         currentPage: 'dashboard'
     });
 });
 
 router.get("/clientes", verificarAutenticacao, (req, res) => {
-    res.render("clientes", {
+    res.render("clientes/cliente-lista", {
         user: req.session.user,
         currentPage: 'clientes'
     });
 });
 
 router.get("/clientes/novo", verificarAutenticacao, (req, res) => {
-    res.render("cliente-novo", {
+    res.render("clientes/cliente-form", {
         user: req.session.user,
         currentPage: 'clientes'
     });
 });
 
 router.get("/servicos", verificarAutenticacao, (req, res) => {
-    res.render("servicos", {
+    res.render("servicos/servico-lista", {
         user: req.session.user,
         currentPage: 'servicos'
     });
 });
 
 router.get("/servicos/novo", verificarAutenticacao, (req, res) => {
-    res.render("servico-novo", {
+    res.render("servicos/servico-form", {
         user: req.session.user,
         currentPage: 'servicos'
     });
 });
 
 router.get("/ordem-servico", verificarAutenticacao, (req, res) => {
-    res.render("ordem-servico", {
+    res.render("servicos/os-lista", {
         user: req.session.user,
         currentPage: 'ordemservicos'
     });
 });
 
 router.get("/ordem-servico/novo", verificarAutenticacao, (req, res) => {
-    res.render("ordem-servico-novo", {
+    res.render("servicos/os-form", {
         user: req.session.user,
         currentPage: 'ordem-servicos-novo'
     });
@@ -87,35 +87,35 @@ router.get("/scrum-board", verificarAutenticacao, (req, res) => {
     });
 });
 router.get("/usuario", verificarAutenticacao, (req, res) => {
-    res.render("usuario", {
+    res.render("usuarios/usuario-lista", {
         user: req.session.user,
         currentPage: 'usuario'
     });
 });
 
 router.get("/usuario/novo", verificarAutenticacao, (req, res) => {
-    res.render("usuario-novo", {
+    res.render("usuarios/usuario-form", {
         user: req.session.user,
         currentPage: 'usuario-novo'
     });
 });
 
 router.get("/atividades", verificarAutenticacao, (req, res) => {
-    res.render("atividades", {
+    res.render("usuarios/atividades", {
         user: req.session.user,
         currentPage: 'atividades'
     });
 });
 
 router.get("/perfil", verificarAutenticacao, (req, res) => {
-    res.render("perfil", {
+    res.render("usuarios/perfil-lista", {
         user: req.session.user,
         currentPage: 'perfil'
     });
 });
 
 router.get("/perfil/novo", verificarAutenticacao, (req, res) => {
-    res.render("perfil-novo", {
+    res.render("usuarios/perfil-form", {
         user: req.session.user,
         currentPage: 'perfil-novo'
     });
@@ -124,46 +124,48 @@ router.get("/perfil/novo", verificarAutenticacao, (req, res) => {
 
 
 router.get("/risco", verificarAutenticacao, (req, res) => {
-    res.render("risco", {
+    res.render("estoque/risco-lista", {
         user: req.session.user,
         currentPage: 'risco'
     });
 });
 
 router.get("/risco/novo", verificarAutenticacao, (req, res) => {
-    res.render("risco-novo", {
+    res.render("estoque/risco-form", {
         user: req.session.user,
         currentPage: 'risco-novo'
     });
 });
 
-router.get("/epc", verificarAutenticacao, (req, res) => {
-    res.render("epc", {
-        user: req.session.user,
-        currentPage: 'epc'
-    });
-});
-
-router.get("/epc/novo", verificarAutenticacao, (req, res) => {
-    res.render("epc-novo", {
-        user: req.session.user,
-        currentPage: 'epc-novo'
-    });
-});
-
 router.get("/epi", verificarAutenticacao, (req, res) => {
-    res.render("epis", {
+    res.render("estoque/epis-lista", {
         user: req.session.user,
         currentPage: 'epis'
     });
 });
 
 router.get("/epi/novo", verificarAutenticacao, (req, res) => {
-    res.render("epis-novo", {
+    res.render("estoque/epis-form", {
         user: req.session.user,
         currentPage: 'epis-novo'
     });
 });
+
+router.get("/epc", verificarAutenticacao, (req, res) => {
+    res.render("estoque/epc-lista", {
+        user: req.session.user,
+        currentPage: 'epc'
+    });
+});
+
+router.get("/epc/novo", verificarAutenticacao, (req, res) => {
+    res.render("estoque/epc-form", {
+        user: req.session.user,
+        currentPage: 'epc-novo'
+    });
+});
+
+
 
 
 router.get("/logout", (req, res) => {
